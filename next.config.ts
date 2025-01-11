95bf98edbaba870a1877b5215d0b847126b92ac1
Abel Moro <abel.moro@educa.madrid.org>
2025-01-11 21:40:30 +0100

    vopilot changes

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/abelmoroeducamadrid.github.io' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/abelmoroeducamadrid.github.io' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/abelmoroeducamadrid.github.io/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/abelmoroeducamadrid.github.io/' : '',
 }
 
 module.exports = nextConfig
