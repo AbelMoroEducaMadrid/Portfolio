93f07eae4d31a131e46be20d25b1d05504f0465f
Abel Moro <abel.moro@educa.madrid.org>
2025-01-13 02:35:33 +0100

    Revert "resolving some minor page errors"
    This reverts commit bb8d9ae7d29f9dc8609c7daddbca177c7df8df5e.

--- app/components/AnimatedBackground.tsx
+++ app/components/AnimatedBackground.tsx
@@ -32,8 +32,8 @@ export default function AnimatedBackground() {
       color: string
 
       constructor() {
-        this.x = Math.random() * canvas.width
-        this.y = Math.random() * canvas.height
+        this.x = Math.random() * canvas!.width
+        this.y = Math.random() * canvas!.height
         this.size = Math.random() * 3 + 1
         this.speedX = Math.random() * 1 - 0.5
         this.speedY = Math.random() * 1 - 0.5
@@ -55,18 +55,18 @@ export default function AnimatedBackground() {
         this.speedX *= 0.95
         this.speedY *= 0.95
 
-        if (this.x > canvas.width) this.x = 0
-        else if (this.x < 0) this.x = canvas.width
-        if (this.y > canvas.height) this.y = 0
-        else if (this.y < 0) this.y = canvas.height
+        if (this.x > canvas!.width) this.x = 0
+        else if (this.x < 0) this.x = canvas!.width
+        if (this.y > canvas!.height) this.y = 0
+        else if (this.y < 0) this.y = canvas!.height
       }
 
       draw() {
-        ctx.fillStyle = this.color
-        ctx.beginPath()
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
-        ctx.closePath()
-        ctx.fill()
+        ctx!.fillStyle = this.color
+        ctx!.beginPath()
+        ctx!.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+        ctx!.closePath()
+        ctx!.fill()
       }
     }
 
@@ -77,7 +77,7 @@ export default function AnimatedBackground() {
     }
 
     function animate() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      ctx!.clearRect(0, 0, canvas!.width, canvas!.height)
       for (let i = 0; i < particles.length; i++) {
         particles[i].update()
         particles[i].draw()
@@ -90,12 +90,12 @@ export default function AnimatedBackground() {
       particles.forEach(p2 => {
         const distance = Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2)
         if (distance < connectionDistance) {
-          ctx.strokeStyle = `rgba(${parseInt(p1.color.slice(4, 7))}, ${parseInt(p1.color.slice(9, 12))}, ${parseInt(p1.color.slice(14, 17))}, ${1 - distance / connectionDistance})`
-          ctx.lineWidth = 1
-          ctx.beginPath()
-          ctx.moveTo(p1.x, p1.y)
-          ctx.lineTo(p2.x, p2.y)
-          ctx.stroke()
+          ctx!.strokeStyle = `rgba(${parseInt(p1.color.slice(4, 7))}, ${parseInt(p1.color.slice(9, 12))}, ${parseInt(p1.color.slice(14, 17))}, ${1 - distance / connectionDistance})`
+          ctx!.lineWidth = 1
+          ctx!.beginPath()
+          ctx!.moveTo(p1.x, p1.y)
+          ctx!.lineTo(p2.x, p2.y)
+          ctx!.stroke()
         }
       })
     }
@@ -104,9 +104,8 @@ export default function AnimatedBackground() {
     animate()
 
     const handleResize = () => {
-      canvas.width = window.innerWidth
-      canvas.height = window.innerHeight
-      init() // Reinitialize particles on resize
+      canvas!.width = window.innerWidth
+      canvas!.height = window.innerHeight
     }
 
     const handleMouseMove = (event: MouseEvent) => {
